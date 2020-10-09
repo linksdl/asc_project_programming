61f9b61f8b9607ae2d8ba5a58a7172eb6efacbe0
linksdl <794469928@qq.com>
2020-10-09 22:38:31 +0100

    Numpy Learning

---
+++ 2-numpy tutorials/15-Numpy Random.py
@@ -0,0 +1,15 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@Project : asc-project-learning_python      
+@File    : 15-Numpy Random.py
+@Author  : Billy Sheng 
+@Contact : shengdl999links@gmail.com  
+@Date    : 2020/10/9 10:35 下午
+@Version  : 1.0.0
+@License : Apache License 2.0
+@Desc    : None
+"""
+
+
